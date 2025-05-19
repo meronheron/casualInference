339adc70e9f8c7434e14b3e306b2167b6f643e1e
meronheron <merongetaneh.sel9@gmail.com>
2025-05-19 14:35:04 +0300

    initialize and fit XLearner

--- dml_infernce.py
+++ dml_infernce.py
@@ -6,3 +6,17 @@ from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 np.random.seed(42)
 # loading dataset
 df = pd.read_csv("dml_kaggle.csv")
+#define features, treatment, and outcome
+X = df[["Quantity", "Price", "Customer ID"]]  # Covariates + control
+T = df["T"]  # Treatment (1: UK, 0: non-UK)
+y = df["Y"]  # Outcome (total purchase amount)
+# initialize XLearner
+x_learner = XLearner(
+    learner=RandomForestRegressor(n_estimators=100, random_state=42),
+    control_learner=RandomForestRegressor(n_estimators=100, random_state=42),
+    treatment_learner=RandomForestRegressor(n_estimators=100, random_state=42),
+    propensity_model=RandomForestClassifier(n_estimators=100, random_state=42)
+)
+
+# fit XLearner
+x_learner.fit(y, T, X=X)
