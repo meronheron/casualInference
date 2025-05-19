1460ca403c425485e55fd39cc9e418239ff76aad
meronheron <merongetaneh.sel9@gmail.com>
2025-05-19 11:44:05 +0300

    remove missing Customer ID, cancellations

--- dml_dataset.py
+++ dml_dataset.py
@@ -2,3 +2,10 @@ import pandas as pd
 import numpy as np
 # setting random seed for reproducibility
 np.random.seed(42)
+# read the excel dataset
+df = pd.read_excel("online_retail_II.xlsx")
+# Clean data: remove missing Customer ID, cancellations
+print("Original rows:", len(df))
+df = df.dropna(subset=["Customer ID"])
+print("Rows after dropping missing Customer ID:", len(df))
+df = df[~df["Invoice"].str.startswith("C", na=False)]
