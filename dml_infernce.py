8e2f4640fa27100fd0f9fbcb44cff2e0904aa903
meronheron <merongetaneh.sel9@gmail.com>
2025-05-19 14:32:21 +0300

    load csv dataset

--- dml_infernce.py
+++ dml_infernce.py
@@ -2,3 +2,7 @@ import pandas as pd
 import numpy as np
 from causalml.inference.meta import XLearner
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+#Setting random seed for reproducibility
+np.random.seed(42)
+# loading dataset
+df = pd.read_csv("dml_kaggle.csv")
